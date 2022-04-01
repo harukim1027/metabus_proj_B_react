0c8d350b1ac80a55dfff6fb12135139d798b6f6e
Yoona Kim <93593904+yoonakim1027@users.noreply.github.com>
2022-04-01 16:35:27 +0900

    HallOfFame css 수정

--- src/Components/review/HallOfFame.jsx
+++ src/Components/review/HallOfFame.jsx
@@ -52,17 +52,24 @@ function Fame() {
 
   return (
     <>
-      <div className="header flex flex-wrap justify-center">
-        <div className="mx-1 review_header rounded-xl shadow-md overflow-hidden xs:px-0 sm:px-20 pt-5 pb-10 my-1 w-2/3 lg:w-2/3 md:w-5/6 sm:w-full xs:w-full">
-          <blockquote className="mt-2 font-semibold italic text-center text-slate-900">
-            <span className="mt-7 mb-6 before:block before:absolute before:-inset-1 before:bg-yellow-400 relative inline-block xs:text-l sm:text-xl lg:text-2xl">
+      <div className="header flex flex-wrap justify-center mt-10">
+        <div className="mx-1 review_header rounded-xl shadow-md overflow-hidden xs:px-0 sm:px-20 pt-10 pb-10 my-1 w-2/3 lg:w-2/3 md:w-5/6 sm:w-full xs:w-full">
+          <blockquote className="mt-5 font-semibold italic text-center text-slate-900">
+            <span className="mt-7 mb-6 before:block before:absolute before:-inset-1 before:-skew-y-3 before:bg-yellow-400 relative inline-block  xs:text-2xl sm:text-4xl lg:text-6xl">
               <span className="relative text-white">" 명예의 전당 "</span>
             </span>
-            <h1 className="text-xs">
-              명예의 전당은 다이어리 글 작성 수로 랭킹이 정해집니다.
-            </h1>
           </blockquote>
-          <hr />
+          <div className="xs:text-xs md:text-base mb-5 text-center">
+            <span>명예의 전당은 다이어리&nbsp;</span>
+            <span
+              className="bg-yellow-100 font-bold"
+              style={{ 'text-decoration': 'underline' }}
+            >
+              글 작성 수
+            </span>
+            <span>로 랭킹이 정해집니다 😆</span>
+          </div>
+          <hr className="mb-5 mt-5" />
           <div className="flex xl:justify-end xs:justify-center">
             {loading && (
               <LoadingIndicator>&nbsp;&nbsp;로딩 중...</LoadingIndicator>
@@ -82,9 +89,10 @@ function Fame() {
               className="Container2"
               mobileTouch={true}
               organicArrows={true}
+              bullets={false}
             >
               <h1 className="flex flex-wrap justify-center space-x-4">
-                <img src="/rank1.png" alt="rank1" className="w-12 h-12" />
+                <img src="/rank1.png" alt="rank1" className="icon_size2" />
                 <h1 className="font-semibold italic">
                   <h1 className="underline decoration-wavy decoration-yellow-400 text-center">
                     {getSortedArr(userArr)[0]?.[0]}
@@ -93,7 +101,7 @@ function Fame() {
                 </h1>
               </h1>
               <h2 className="flex flex-wrap justify-center space-x-4">
-                <img src="/rank2.png" alt="rank2" className="w-12 h-12" />
+                <img src="/rank2.png" alt="rank2" className="icon_size2" />
                 <h1 className="font-semibold italic">
                   <h1 className="underline decoration-wavy decoration-gray-400 text-center">
                     {getSortedArr(userArr)[1]?.[0]}
@@ -103,7 +111,7 @@ function Fame() {
               </h2>
 
               <h3 className="flex flex-wrap justify-center space-x-4">
-                <img src="/rank3.png" alt="rank3" className="w-12 h-12" />
+                <img src="/rank3.png" alt="rank3" className="icon_size2" />
                 <h1 className="font-semibold italic">
                   <h1 className="underline decoration-wavy decoration-yellow-600 text-center">
                     {getSortedArr(userArr)[2]?.[0]}
