8c8e1fda704120479f800a822ee413efc5a9ecbe
Yeol Choi <chlduf153@gmail.com>
2022-04-07 16:49:39 +0900

    소개 페이지 스크롤 이벤트 css 수정

--- src/hooks/useScrollFadeIn.js
+++ src/hooks/useScrollFadeIn.js
@@ -9,10 +9,6 @@ function useScrollFadeIn(direction = 'up', duration = 1, delay = 0) {
         return 'translate3d(0, 50%, 0)';
       case 'down':
         return 'translate3d(0, -50%, 0)';
-      case 'left':
-        return 'translate3d(50%, 0, 0)';
-      case 'right':
-        return 'translate3d(-50%, 0, 0)';
       default:
         return;
     }
@@ -22,6 +18,8 @@ function useScrollFadeIn(direction = 'up', duration = 1, delay = 0) {
     ([entry]) => {
       const { current } = element;
       if (entry.isIntersecting) {
+        current.style.transitionProperty = 'all';
+        current.style.transitionDuration = `${duration}s`;
         current.style.transitionTimingFunction = 'cubic-bezier(0, 0, 0.2, 1)';
         current.style.transitionDelay = `${delay}s`;
         current.style.opacity = 1;
@@ -33,13 +31,13 @@ function useScrollFadeIn(direction = 'up', duration = 1, delay = 0) {
 
   useEffect(() => {
     let observer;
+    const { current } = element;
 
-    if (element.current) {
-      observer = new IntersectionObserver(onScroll, { threshold: 0.7 });
-      observer.observe(element.current);
+    if (current) {
+      observer = new IntersectionObserver(onScroll, { threshold: 1 });
+      observer.observe(current);
+      return () => observer && observer.disconnect();
     }
-
-    return () => observer && observer.disconnect();
   }, [onScroll]);
 
   return {
